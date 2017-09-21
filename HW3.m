1abd6fca27773f42ecc5b701a1d555c244079645
Aryeh <warmflasha@gmail.com>
2017-09-21 13:17:47 -0500

    wrote HW3

---
+++ HW3.m
@@ -0,0 +1,43 @@
+%HW3
+
+%% Problem 1 - Smith-Waterman alignment
+% Consider two sequences 'GTAATCC' and 'GTATCCG'
+
+% Construct the scoring matrix for this with the parameters:
+% match value = 2, mismatch value = -1, and gap penalty = -1. Use your
+% solution to get the optimal alignment. If you prefer, it is acceptable to do this with
+% pencil and paper, you can then take a snapshot of your solution and
+% include it in your repository. 
+
+%% Problem 2 - using the NCBI databases and sequence alignments
+
+% Erk proteins are critical signal transducers of MAP kinase signaling.
+% Accessions numbers for ERK1 (also called MAPK3) and ERK2 (also called MAPK1) human mRNA are NM_002746 and
+% NM_002745, respectively. 
+
+% Part 1. Perform an alignment of the coding DNA sequences of ERK1 and
+% ERK2. What fraction of base pairs in ERK1 can align to ERK2? 
+
+% Part2. Perform an alignment of the aminoacid sequences of ERK1 and ERK2.
+% What fraction of amino acids align?
+
+% Part 3.  Use the NCBI tools to get mRNA sequences for the mouse genes ERK1 and
+% ERK2 and align both the coding DNA sequences and protein sequences to the
+% human versions. How similar are they? 
+
+%% Problem 3: using blast tools programatically
+
+% Part 1. Write a function that takes an NCBI accession number and a number N as input and
+% returns a cell array of the accession numbers for the top N blast hits. 
+
+% Part 2. Write a function that takes an accession number as input, calls your function 
+% from part 1, and returns two outputs - the closest scoring match in human DNA/RNA and the
+% closest non-human match. Hint: see the "Source" or "SourceOrganism" field in the data
+% returned by getgenbank. Make sure your function does something sensible
+% if nothing human is found. 
+
+% Part 3. Choose at least one gene from the human genome and one gene from
+% another organism and run your code from part 2 on the relevant accession
+% numbers. Comment on the results. 
+
+
